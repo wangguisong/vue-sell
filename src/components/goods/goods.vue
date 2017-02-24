81451fb8ef0c81c34b0e2bd316a2fcaf3da61b19
zhaopan <zhaopan@zpMac.local>
2017-02-24 21:01:57 +0800

    add goods 001.

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,12 +1,62 @@
 <template>
-  <div class="menu-wrapper">
+  <div class="goods">
+    <div class="menu-wrapper">
+      <ul>
+        <li class="menu-item" v-for="item in goods">
+          <span class="text">
+            <span class="icon" v-show="item.type>0" :class="classMap[item.type]"/>
+            {{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
+    <div class="foods-wrapper"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  const ERR_OK = 0;
+
+  export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        goods: []
+      };
+    },
+    created() {
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.goods = response.data;
+          console.log(this.goods);
+        }
+      });
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow: hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+      .menu-item
+        display table
+        height 54px
+        width 56px
+    .foods-wrapper
+      flex 1
 </style>
